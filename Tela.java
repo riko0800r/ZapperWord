7583071a9357ba62c7fabae6506b7a3e61ad5ae0
Riko <113864095+riko0800r@users.noreply.github.com>
2022-09-19 13:34:11 -0300

    Add files via upload

--- Tela.java
+++ Tela.java
@@ -1,8 +1,10 @@
 import Java.util.Scanner;
+import java.awt.*;
+import javax.swing.*;
 public class jogo{
-  
   Scanner sc1 = new Scanner(System.in);
   System.out.println("digite o nome personagem");
+  
   String nome = sc1.nextLine();
   System.out.println("o seu nome é" + nome);
   
